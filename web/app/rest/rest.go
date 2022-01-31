592f13f75998db90a4881c3e3492383c9c2fd231
Akshay Misra <Akshay.Misra@tecnotree.com>
2022-01-31 11:46:35 +0530

    added /sumpositive end point to return -1 if any number is negative

--- web/app/rest/rest.go
+++ web/app/rest/rest.go
@@ -13,31 +13,13 @@ func Register() {
 	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			var numbers []int
-			var error string
-			query := r.URL.Query()
-			for key, value := range query {
-				if value == nil || (len(value) > 0 && value[0] == "") {
-					error = "Value for " + key + " is not provided"
-					w.WriteHeader(http.StatusBadRequest)
-				} else if len(value) > 1 {
-					error = "Value for " + key + " is provided more than once."
-					w.WriteHeader(http.StatusBadRequest)
-				} else {
-					fmt.Println("Value for key: " + key + ", Value: " + value[0])
-					number, err := strconv.ParseInt(value[0], 10, 0)
-					if err != nil {
-						error = "Invalid number: " + value[0]
-						w.WriteHeader(http.StatusBadRequest)
-					} else {
-						numbers = append(numbers, int(number))
-					}
-				}
 
-				if error != "" {
-					json.NewEncoder(w).Encode(error)
-					return
-				}
+			numbers, err := getNumbersToAdd(r)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				json.NewEncoder(w).Encode(err.Error())
+
+				return
 			}
 
 			if len(numbers) > 0 {
@@ -52,6 +34,67 @@ func Register() {
 		}
 	})
 
+	http.HandleFunc("/sumpositive", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+
+			numbers, err := getNumbersToAdd(r)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				json.NewEncoder(w).Encode(err.Error())
+
+				return
+			}
+
+			if len(numbers) > 0 {
+				for _, number := range numbers {
+					if number < 0 {
+						w.WriteHeader(http.StatusOK)
+						json.NewEncoder(w).Encode(-1)
+
+						return
+					}
+				}
+
+				sum := add.AddAll(numbers)
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(sum)
+			}
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+			message := "Method not supported"
+			json.NewEncoder(w).Encode(message)
+		}
+	})
+
 	http.ListenAndServe(":3000", nil)
 	fmt.Println("Server is ready in port 3000")
+}
+
+func getNumbersToAdd(request *http.Request) ([]int, error) {
+	var numbers []int
+	var errr string
+	query := request.URL.Query()
+	for key, value := range query {
+		if value == nil || (len(value) > 0 && value[0] == "") {
+			errr = "Value for " + key + " is not provided"
+		} else if len(value) > 1 {
+			errr = "Value for " + key + " is provided more than once."
+		} else {
+			fmt.Println("Value for key: " + key + ", Value: " + value[0])
+			number, err := strconv.ParseInt(value[0], 10, 0)
+			if err != nil {
+				errr = "Invalid number: " + value[0]
+			} else {
+				numbers = append(numbers, int(number))
+			}
+		}
+
+		if errr != "" {
+			return nil, fmt.Errorf(errr)
+		}
+	}
+
+	fmt.Println("getNumbersToAdd, number: ", numbers)
+	return numbers, nil
 }
